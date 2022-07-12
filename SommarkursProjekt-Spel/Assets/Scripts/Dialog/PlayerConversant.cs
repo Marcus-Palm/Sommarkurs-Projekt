8807c10d966be02917ed0f07072423a9294673e7
MackanPalm <marcuspalm@telia.com>
2022-07-12 21:47:12 +0200

    day

---
+++ SommarkursProjekt-Spel/Assets/Scripts/Dialog/PlayerConversant.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace summerProject.Dialogue
+{
+    public class PlayerConversant : MonoBehaviour
+    {
+
+        [SerializeField] Dialogue currentDialogue;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
