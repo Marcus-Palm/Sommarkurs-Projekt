945cca0a03e2ade97658c5b1604dab7448c59097
MackanPalm <marcuspalm@telia.com>
2022-07-16 22:46:51 +0200

    added ghosts and triggerable dialogue

--- SommarkursProjekt-Spel/Assets/Scripts/Dialog/AIConversant.cs
+++ SommarkursProjekt-Spel/Assets/Scripts/Dialog/AIConversant.cs
@@ -2,17 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIConversant : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace summerProject.Dialogue
+{
+
+    public class AIConversant : MonoBehaviour
     {
+        [SerializeField] Dialogue dialogue;
         
+
+        [SerializeField] GameObject conversationPrompt;
+
+        GameObject player;
+
+        bool inConverastionRange = false;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (inConverastionRange && Input.GetKeyDown(KeyCode.E))
+            {
+                player.gameObject.GetComponent<PlayerConversant>().StartDialogue(dialogue);
+            }
+        }
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.tag == "Player" && dialogue != null)
+            {
+                inConverastionRange = true;
+
+                conversationPrompt.SetActive(true);
+
+                player = collision.gameObject;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if(collision.tag == "Player" && conversationPrompt.activeInHierarchy)
+            {
+                inConverastionRange = false;
+
+                conversationPrompt.SetActive(false);
+
+            }
+        }
     }
 }
