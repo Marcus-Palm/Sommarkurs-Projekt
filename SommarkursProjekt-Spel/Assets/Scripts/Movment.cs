45b5e4fac6bb0be433ed334d3475de484d678996
MackanPalm <marcuspalm@telia.com>
2022-07-19 06:24:41 +0200

    yes

--- SommarkursProjekt-Spel/Assets/Scripts/Movment.cs
+++ SommarkursProjekt-Spel/Assets/Scripts/Movment.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,33 +14,69 @@ public class Movment : MonoBehaviour
 
     Vector2 movement;
 
+    public bool mapView = true;
+
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        if(movement.magnitude > 1f)
+
+        if (Input.GetKeyDown(KeyCode.H))
         {
-            movement = movement.normalized;
+            SwitchPlane();
         }
-
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
-
-        if(movement.x == 1f || movement.x == -1f || movement.y == 1f || movement.y == -1f)
+        if (mapView)
         {
-            animator.SetFloat("LastFacingX", movement.x);
-            animator.SetFloat("LastFacingY", movement.y);
+
+            movement.x = Input.GetAxisRaw("Horizontal");
+        
+            movement.y = Input.GetAxisRaw("Vertical");
+            if(movement.magnitude > 1f)
+            {
+                movement = movement.normalized;
+            }
+
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+
+            if(movement.x == 1f || movement.x == -1f || movement.y == 1f || movement.y == -1f)
+            {
+                animator.SetFloat("LastFacingX", movement.x);
+                animator.SetFloat("LastFacingY", movement.y);
+            }
+        }
+        else
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+
+            
+
         }
         
 
     }
 
+    private void SwitchPlane()
+    {
+        if (mapView)
+        {
+            mapView = false;
+            rb.gravityScale = 1;
+        }
+        else
+        {
+            mapView = true;
+            rb.gravityScale = 0;
+        }
+    }
+
     private void FixedUpdate()
     {
-
-        rb.MovePosition(rb.position + movement * movmentSpeed * Time.fixedDeltaTime);
+        
+        
+            rb.MovePosition(rb.position + movement * movmentSpeed * Time.fixedDeltaTime);
+        
+            
     }
 }
